aaee84243c7206507e5f175888a510428ecb2fad
Lizzy <36128102+LizzyXR@users.noreply.github.com>
2025-12-08 16:43:06 +0100

    Refactor install script to remove repo cloning
    Removed cloning of the repository and directly copied configuration files.

--- install.sh
+++ install.sh
@@ -5,19 +5,15 @@ set -euo pipefail
 echo "Installing required packages..."
 sudo pacman -S --needed niri waybar mako fuzzel fastfetch swaylock swayidle
 
-echo "Cloning repo..."
-TMPDIR=$(mktemp -d)
-git clone https://github.com/LizzyXR/laptop-rice.git "$TMPDIR"
-
 echo "Performing magic..."
 mkdir -p "$HOME/.config"
-cp -r "$TMPDIR"/niri "$HOME/.config/"
-cp -r "$TMPDIR"/waybar "$HOME/.config/"
-cp -r "$TMPDIR"/mako "$HOME/.config/"
-cp -r "$TMPDIR"/fuzzel "$HOME/.config/"
-cp -r "$TMPDIR"/fastfetch "$HOME/.config/"
-cp -r "$TMPDIR"/swaylock "$HOME/.config/"
-cp -r "$TMPDIR"/swayidle "$HOME/.config/"
+cp -r niri "$HOME/.config/"
+cp -r waybar "$HOME/.config/"
+cp -r mako "$HOME/.config/"
+cp -r fuzzel "$HOME/.config/"
+cp -r fastfetch "$HOME/.config/"
+cp -r swaylock "$HOME/.config/"
+cp -r swayidle "$HOME/.config/"
 
 echo "Cleaning up..."
 rm -rf "$TMPDIR"
