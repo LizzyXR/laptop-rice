0ad8666b75ef919a8c24fb46f05673bd7d3e73a2
Lizzy <36128102+LizzyXR@users.noreply.github.com>
2025-12-08 16:37:19 +0100

    Add power menu script using Fuzzel
    Complete rework of the power menu

---
+++ waybar/menus/power_menu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Power menu using Fuzzel
+CHOICE=$(printf "Suspend\nHibernate\nShutdown\nReboot" | fuzzel --dmenu --prompt "Power:")
+
+case $CHOICE in
+    Suspend) systemctl suspend ;;
+    Hibernate) systemctl hibernate ;;
+    Shutdown) systemctl poweroff ;;
+    Reboot) systemctl reboot ;;
+esac
