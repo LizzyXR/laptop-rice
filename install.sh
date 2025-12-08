3ad4b38605aa7e7cc14cf30d7fd417d7c8fda672
Lizzy <36128102+LizzyXR@users.noreply.github.com>
2025-12-08 16:55:41 +0100

    Enhance install.sh with permissions and reboot prompt
    Added permission changes for waybar menu scripts and a reboot prompt.

--- install.sh
+++ install.sh
@@ -13,4 +13,15 @@ cp -r mako "$HOME/.config/"
 cp -r fuzzel "$HOME/.config/"
 cp -r fastfetch "$HOME/.config/"
 
-echo "Done! :D"
+echo "Applying correct permissions to waybar menu's..."
+chmod +x ~/.config/waybar/indicators/executable_bluetooth-manager.sh
+chmod +x ~/.config/waybar/indicators/executable_bluetooth-toggle.sh
+chmod +x ~/.config/waybar/indicators/executable_power-profiles.sh
+chmod +x ~/.config/waybar/indicators/executable_screen-recording.sh
+
+read -p "Done! Reboot? [Y/n]: " answer
+answer=${answer:-Y}
+case "$answer" in
+    [Yy]* ) systemctl reboot;;
+    [Nn]* ) echo "No"; exit 0;;
+esac
