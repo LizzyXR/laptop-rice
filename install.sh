d643cb6def0e06cd0e9f3160423b66b0176841e1
Lizzy <36128102+LizzyXR@users.noreply.github.com>
2025-12-10 00:48:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,27 +2,32 @@
 
 set -euo pipefail
 
-echo "Installing required packages..."
-sudo pacman -S --needed --noconfirm niri waybar mako fuzzel fastfetch swaylock swayidle calcurse gammastep rust libpipewire pkgconf clang
-cargo install wiremix
-
-echo "Performing magic..."
-mkdir -p "$HOME/.config"
-cp -r niri "$HOME/.config/"
-cp -r waybar "$HOME/.config/"
-cp -r mako "$HOME/.config/"
-cp -r fuzzel "$HOME/.config/"
-cp -r fastfetch "$HOME/.config/"
-
-echo "Applying correct permissions to waybar menu's..."
-chmod +x ~/.config/waybar/indicators/executable_bluetooth-manager.sh
-chmod +x ~/.config/waybar/indicators/executable_bluetooth-toggle.sh
-chmod +x ~/.config/waybar/indicators/executable_power-profiles.sh
-chmod +x ~/.config/waybar/indicators/executable_screen-recording.sh
-
-read -p "Done! Reboot? [Y/n]: " answer
-answer=${answer:-Y}
-case "$answer" in
-    [Yy]* ) systemctl reboot;;
-    [Nn]* ) echo "No"; exit 0;;
-esac
+if command -v pacman >/dev/null 2>&1; then
+    echo "Installing required packages..."
+    sudo pacman -S --needed --noconfirm niri waybar mako fuzzel fastfetch swaylock swayidle calcurse gammastep rust libpipewire pkgconf clang
+    cargo install wiremix
+    
+    echo "Performing magic..."
+    mkdir -p "$HOME/.config"
+    cp -r niri "$HOME/.config/"
+    cp -r waybar "$HOME/.config/"
+    cp -r mako "$HOME/.config/"
+    cp -r fuzzel "$HOME/.config/"
+    cp -r fastfetch "$HOME/.config/"
+    
+    echo "Applying correct permissions to waybar menu's..."
+    chmod +x ~/.config/waybar/indicators/executable_bluetooth-manager.sh
+    chmod +x ~/.config/waybar/indicators/executable_bluetooth-toggle.sh
+    chmod +x ~/.config/waybar/indicators/executable_power-profiles.sh
+    chmod +x ~/.config/waybar/indicators/executable_screen-recording.sh
+    
+    read -p "Done! Reboot? [Y/n]: " answer
+    answer=${answer:-Y}
+    case "$answer" in
+        [Yy]* ) systemctl reboot;;
+        [Nn]* ) echo "No"; exit 0;;
+    esac
+else
+    echo "This rice is unfortunately only available for Arch based systems :("
+    exit 1
+fi
